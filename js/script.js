532bf1e22b6dd29884e30cae35ee82b5d41d917e
tinkershop <33951216+tinkershop@users.noreply.github.com>
2017-11-28 10:30:52 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,34 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId            : '1760730183939339',
-    autoLogAppEvents : true,
-    xfbml            : true,
-    version          : 'v2.11'
-  });
-
-  FB.api(
-    '/17841400971875220/media',
-    'GET',
-    {"fields":"media_url,shortcode,caption", "access_token":"EAAZABXZCEotQsBAGDQsfTv9TCjhgPIJlJTSWLujgdU7jvnWq5mPyBPsxeDSjZB9jtCPfOzxQelFJsW5ttaoULDSjOocptDn4CanOwm7JveP2t1FAecFYeSwu0ZCXDCUE09CQAU9lUpYRRZBPrP6SxWM60xt8T4TLTeVkFYhRNeQZDZD"},
-    function(response) {
-        console.log(response);
-        jsonWrite();
-    }
-  );
-};
-
-(function(d, s, id){
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function jsonWrite() {
+jsonRead();
+function jsonRead() {
   var obj = {
      table: []
   };
 
   obj.table.push({id: 1, square:2});
   var json = JSON.stringify(obj);
-  var url = './data/instagram.json';
+  var url = 'https://tinkershop.github.io/data/instagram.json';
 
   $.getJSON(url, function( data ) {
-    console.log(data);
     var items = [];
     $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
+      $.each( val, function( key1, val1 ) {
+        items.push(
+          '<div class="col-sm-6 col-md-4">' +
+            '<div class="panel panel-default">' +
+              '<div class="panel-heading"></div>' +
+                '<div class="panel-body panel-instagram">' +
+                  '<img src="' + val1.media_url + '" style="width: 100%;"></img>' +
+                '</div>' +
+              '<div class="panel-footer"></div>' +
+            '</div>' +
+          '</div>'
+        );
+      });
     });
    
-    $( "<ul/>", {
-      "class": "my-new-list",
+    $( "<div/>", {
+      "class": "list",
       html: items.join( "" )
-    }).appendTo( "body" );
+    }).prependTo( "#allTab" );
   });
 }

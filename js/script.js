204964f1127ec2177fcb5e8795e5371102780894
tinkershop <33951216+tinkershop@users.noreply.github.com>
2017-11-28 11:22:27 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 jsonRead();
 function jsonRead() {
-  var obj = {
-     table: []
-  };
-
-  obj.table.push({id: 1, square:2});
-  var json = JSON.stringify(obj);
   var url = 'https://tinkershop.github.io/data/instagram.json';
-
   $.getJSON(url, function( data ) {
     var items = [];
     $.each( data, function( key, val ) {
@@ -16,7 +9,7 @@ function jsonRead() {
           '<div class="col-sm-6 col-md-4">' +
             '<div class="panel panel-default">' +
               '<div class="panel-heading"></div>' +
-                '<div class="panel-body panel-instagram">' +
+                '<div onclick="instagramData(\'https://www.instagram.com/p/'+ val1.shortcode +'\')" class="panel-body panel-instagram">' +
                   '<img src="' + val1.media_url + '" style="width: 100%;"></img>' +
                 '</div>' +
               '<div class="panel-footer"></div>' +
@@ -30,10 +23,52 @@ function jsonRead() {
       "class": "list",
       html: items.join( "" )
     }).prependTo( "#allTab" );
-    
+
     var monkeyList = new List('allTab', {
       page: 9,
       pagination: true
     });
+  });
+}
+
+function instagramData(url) {
+  $('#modalBody').html('<img class="loading-img" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif">');
+  $('#modal-dialog').modal('show');
+        
+  $.getJSON(url+'?__a=1', function( data ) {
+    var info = data.graphql.shortcode_media;
+    var comments = [];
+    $.each( info.edge_media_to_comment.edges, function( key, comment ) {
+      comments.push(
+        '<div>' +
+          '<p><b>'+ comment.node.owner.username +'</b> '+ comment.node.text +'</p>' +
+        '</div>'
+      );
+    });
+
+    $('#modalBody').html(
+      '<div class="row flex">' +
+        '<div class="col-md-7 insta-img">' +
+            '<img class="w100" src="'+ info.display_url +'">' +
+        '</div>' +
+        '<div class="col-md-5 insta-data">' +
+          '<div class="insta-profile">' +
+           '<img imageonload="imgloaded()" src="'+ info.owner.profile_pic_url +'">' +
+            '<p>'+ info.owner.username +'</p>' +
+            '<a class="btn btn-primary" href="https://www.instagram.com/p/'+ info.shortcode +'">visit</a>' +
+          '</div>' +
+          '<div class="insta-comments">' +
+            comments +
+          '</div>' +
+          '<div class="insta-likes">' +
+            '<p>'+ info.edge_media_preview_like.count +' likes</p>' +
+          '</div>' +
+        '</div>' +
+      '</div>'
+    );
+
+    setTimeout(function(){
+      $('.insta-data').height($('.insta-img').height());
+    }, 1000);
   });
 }
